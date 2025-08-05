98720a43ebbf6ead4c0eada5d0f8a82680695fd9
antelor <ramiro.antelo4@gmail.com>
2025-08-05 19:59:59 -0300

    added venganza

--- src/Venganza.jsx
+++ src/Venganza.jsx
@@ -4,12 +4,36 @@ import { useState } from 'react';
 import  { useEffect } from "react";
 import { db } from "./firebase";
 import { doc, onSnapshot } from "firebase/firestore";
+import './Venganza.css';
 
+const elements = [
+    "Justicia por Mano Propia",
+    "La sed de sangre ha sido saciada",
+    "Pero la deuda permanece",
+    "El ciclo se repite. En el hormigon,",
+    "el eco de la tragedia resuena sin fin.",
+];
 
 function Venganza() {
+    const [visibleCount, setVisibleCount] = useState(1);
     const [votes, setVotes] = useState({ optionA: 0, optionB: 0 });
-
+    
     const pollRef = doc(db, "polls", "mainPoll");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+        setVisibleCount((prev) => {
+            if (prev >= elements.length) {
+                clearInterval(interval);
+            return prev;
+            }
+            return prev + 1;
+        });
+        }, 2000); // time between fades
+    
+        return () => clearInterval(interval);
+    }, []);
+    
 
     useEffect(() => {
         const unsubscribe = onSnapshot(pollRef, (docSnap) => {
@@ -26,9 +50,15 @@ function Venganza() {
         totalVotes ? ((value / totalVotes) * 100).toFixed(1) : 0;
 
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1>¡Ganó Venganza por mano propia!</h1>
-            <h3>El ciclo de derramado de sangre continua.</h3>
+        <div className="stacked-page2">
+            {elements.map((text, index) => (
+                <div
+                key={index}
+                className={`fade-section ${index < visibleCount ? "visible" : ""}`}
+                >
+                {text}
+                </div>
+            ))}
             <p>
                 Venganza racional: {votes.optionA} votos ({percent(votes.optionA)}%)
             </p>
